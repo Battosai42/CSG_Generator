b1b74e33c6110cfa479462e4588d99d92570943b
Battosai42 <maveric-@gmx.ch>
2018-04-04 20:32:58 +0200

    cleanup

--- python_scripts/ngon.py
+++ python_scripts/ngon.py
@@ -1,53 +1,48 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# basic definitions
 n=100
 r1=20
 r2=50
-alpha = 360/n
 
-x1=[]
-y1=[]
-x1.append(r1)
-y1.append(0)
-for m in range(1, n+1, 1):
-    x1.append(r1 * np.cos(2 * np.pi * m / n))
-    y1.append(r1 * np.sin(2 * np.pi * m / n))
+# definitions
+def createNgon(n, r):
+    x=[r]
+    y=[0]
+    for m in range(1, n+1, 1):
+        x.append(r * np.cos(2 * np.pi * m / n))
+        y.append(r * np.sin(2 * np.pi * m / n))
+    return x, y
+    
+def createCirMeander(n, r1, r2):
+    x1, y1 = createNgon(n, r1)
+    x2, y2 = createNgon(n, r2)
+    x=[]
+    y=[]
+    for i in list(range(0, int(n), 2)):
+        for k in list(range(4)):
+            print(i, k)
+            if k == 0:
+                x.append(x2[i])
+                y.append(y2[i])
+            elif k == 1:
+                x.append(x2[i + k])
+                y.append(y2[i + k])
+            elif k == 2:
+                x.append(x1[i + k - 1])
+                y.append(y1[i + k - 1])
+            elif k == 3:
+                x.append(x1[i + k - 1])
+                y.append(y1[i + k - 1])
+    x.append(x2[0])
+    y.append(y2[0])
+    return x, y
 
-x2=[]
-y2=[]
-x2.append(r2)
-y2.append(0)
-for m in range(1, n+1, 1):
-    x2.append(r2 * np.cos(2 * np.pi * m / n))
-    y2.append(r2 * np.sin(2 * np.pi * m / n))
+# main
+x, y = createCirMeander(n, r1, r2)
 
-x=[]
-y=[]
-k=0
-for i in list(range(0, int(n), 2)):
-    for k in list(range(4)):
-        print(i, k)
-        if k == 0:
-            x.append(x2[i])
-            y.append(y2[i])
-        elif k == 1:
-            x.append(x2[i + k])
-            y.append(y2[i + k])
-        elif k == 2:
-            x.append(x1[i + k - 1])
-            y.append(y1[i + k - 1])
-        elif k == 3:
-            x.append(x1[i + k - 1])
-            y.append(y1[i + k - 1])
-x.append(x2[0])
-y.append(y2[0])
-
-print(alpha)
-print(x1)
-print(y1)
-
-#plt.plot(x1, y1)
-#plt.plot(x2, y2)
+plt.figure(num=1, figsize=(8, 8), dpi=80, facecolor='w', edgecolor='k')
+plt.grid(True, which='both')
 plt.plot(x, y)
 plt.show()
