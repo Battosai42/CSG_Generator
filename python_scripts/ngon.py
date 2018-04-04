e724f8b39f2163a3f4589aaf90f5ffec38c0e244
Battosai42 <maveric-@gmx.ch>
2018-04-04 20:42:51 +0200

    cleanup2

--- python_scripts/ngon.py
+++ python_scripts/ngon.py
@@ -1,13 +1,11 @@
+# author: Battosai42
+
+# imports
 import numpy as np
 import matplotlib.pyplot as plt
 
-# basic definitions
-n=100
-r1=20
-r2=50
-
 # definitions
-def createNgon(n, r):
+def createNgon(n, r):   # create regular n-gon nith n sides and a circumference of r
     x=[r]
     y=[0]
     for m in range(1, n+1, 1):
@@ -15,14 +13,13 @@ def createNgon(n, r):
         y.append(r * np.sin(2 * np.pi * m / n))
     return x, y
     
-def createCirMeander(n, r1, r2):
+def createCirMeander(n, r1, r2):    # create circular meander using 2 regular n-gons (n has to be even)
     x1, y1 = createNgon(n, r1)
     x2, y2 = createNgon(n, r2)
     x=[]
     y=[]
     for i in list(range(0, int(n), 2)):
         for k in list(range(4)):
-            print(i, k)
             if k == 0:
                 x.append(x2[i])
                 y.append(y2[i])
@@ -39,9 +36,16 @@ def createCirMeander(n, r1, r2):
     y.append(y2[0])
     return x, y
 
-# main
+# main()
+
+n=100   
+r1=20
+r2=50
+
+# create circular meander
 x, y = createCirMeander(n, r1, r2)
 
+# plot meander
 plt.figure(num=1, figsize=(8, 8), dpi=80, facecolor='w', edgecolor='k')
 plt.grid(True, which='both')
 plt.plot(x, y)
