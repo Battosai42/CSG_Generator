2f533b586e29820995bf27817f6dcf3dc5d54cba
Battosai42 <33894357+Battosai42@users.noreply.github.com>
2018-04-06 10:32:31 +0200

    added rotation and XY offset

--- python_scripts/ngon.py
+++ python_scripts/ngon.py
@@ -1,25 +1,26 @@
 # author: Battosai42
 
 # imports
-import numpy as np
+import math as ma
 import matplotlib.pyplot as plt
-import pcbnew
+#import pcbnew
 
 # definitions
-def createNgon(n, r):   # create regular n-gon nith n sides and a circumference of r
-    x=[r]
-    y=[0]
-    for m in range(1, n+1, 1):
-        x.append(r * np.cos(2 * np.pi * m / n))
-        y.append(r * np.sin(2 * np.pi * m / n))
-    length = np.sqrt(np.abs(x[1]-x[0])**2+np.abs(y[1]-y[0])**2) # calculate the segment length
+def createNgon(n, r, xoffset=0, yoffset=0):   # create regular n-gon nith n sides and a circumference of r
+    x=[r + xoffset]
+    y=[0 + yoffset]
+    for m in range(1, n, 1):
+        x.append(r * ma.cos(2 * ma.pi * m / n) + xoffset)
+        y.append(r * ma.sin(2 * ma.pi * m / n) + yoffset)
+    length = ma.sqrt(abs(x[1]-x[0])**2+abs(y[1]-y[0])**2) # calculate the segment length
     return x, y, length
     
-def createCirMeander(n, r1, r2):    # create circular meander using 2 regular n-gons (n has to be even)
-    x1, y1, l1 = createNgon(n, r1)
-    x2, y2, l2 = createNgon(n, r2)
+def createCirMeander(n, r1, r2, shift=0, xoffset=0, yoffset=0):    # create circular meander using 2 regular n-gons (n has to be even)
+    x1, y1, l1 = createNgon(n, r1, xoffset, yoffset)
+    x2, y2, l2 = createNgon(n, r2, xoffset, yoffset)
     x=[]
     y=[]
+    j=shift+1
     for i in list(range(0, int(n), 2)):
         for k in list(range(4)):
             if k == 0:
@@ -28,12 +29,13 @@ def createCirMeander(n, r1, r2):    # create circular meander using 2 regular n-
             elif k == 1:
                 x.append(x2[i + k])
                 y.append(y2[i + k])
-            elif k == 2:
-                x.append(x1[i + k - 1])
-                y.append(y1[i + k - 1])
-            elif k == 3:
-                x.append(x1[i + k - 1])
-                y.append(y1[i + k - 1])
+            elif k == 2 or k == 3:
+                if i + k - j < n:       # if the shift is not 0
+                    x.append(x1[i + k - j])
+                    y.append(y1[i + k - j])
+                else:                   # if the shift is 0
+                    x.append(x1[0])
+                    y.append(y1[0])
     x.append(x2[0])
     y.append(y2[0])
     return x, y, [l1, l2]
@@ -54,8 +56,8 @@ def createTracks(x, y):
     for i in range(len(x)):
         t = pcbnew.TRACK(pcb)
         pcb.Add(t)
-        t.SetStart(pcbnew.wxPointMM(round(x[i],2), round(y[i],2))
-        t.SetEnd(pcbnew.wxPointMM(round(x[i+1],2), round(y[i+1],2))
+        t.SetStart(pcbnew.wxPointMM(round(x[i],2), round(y[i],2)))
+        t.SetEnd(pcbnew.wxPointMM(round(x[i+1],2), round(y[i+1],2)))
         t.SetNetCode(i)
         t.SetLayer(31)
         
@@ -74,6 +76,5 @@ r2=50   # circumference of outer ngon
 
 # create circular meander
 n = calcN(r1, wire_width, clearance)    # calculate max n to comply wire with and clearance
-x, y, l = createCirMeander(n, r1, r2)
-createTracks(x, y)
-
+x, y, l = createCirMeander(n, r1, r2, shift=0, xoffset=20, yoffset=20)
+plot(x,y)
